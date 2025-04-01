d6acac2848167bbfac5ea3c768e5b62a62734e42
Xpektra7 <xpektra7@gmail.com>
2025-04-02 00:01:50 +0100

    fix: update image path in Header component and adjust CSS for body and meme styling

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ export default function Header(){
     return(
         <div className="header">
             <div>
-            <img src="../src/assets/troll-face.png" alt="logo" className="logo" />
+            <img src="./assets/troll-face.png" alt="logo" className="logo" />
             <h1>Meme Generator</h1>
             </div>
         </div>
