c00283a96ae0916d313bd9c65451de5fdc8b6044
Xpektra7 <xpektra7@gmail.com>
2025-04-01 01:08:20 +0100

    fix: remove unintended character from Main component

--- src/Main.jsx
+++ src/Main.jsx
@@ -12,7 +12,7 @@ export default function Main() {
          ) )
   }
   return (
-    <div className="main">
+    <div className="main"> 
       <div>
         <label htmlFor="topText">
           Top Text
